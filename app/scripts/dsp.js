5fbe2e02d2e5705a237060581f5bd75aad6ce2cb
Damian Prus <prus.damian@gmail.com>
2015-07-14 19:00:33 +0200

    add filter

--- app/scripts/dsp.js
+++ app/scripts/dsp.js
@@ -5,11 +5,32 @@ angular.module('rumca-js')
   	var ctx = new $window.AudioContext();
   	var voices = [];
 
+    //init filter
+    var filter = ctx.createBiquadFilter();
+    filter.type = 'lowpass';
+    filter.Q.value = 7.0;
+    filter.frequency.value = 22050;
+    filter.connect(ctx.destination);
+
+    //init osc1 gain
+    var osc1Gain = ctx.createGain();
+    osc1Gain.connect(filter);
+
+    //inti osc2 gain
+    var osc2Gain = ctx.createGain();
+    osc2Gain.connect(filter);
+
     return {
     	ctx: ctx,
-      osc: {
+      osc1: {
         type: 'sine'
       },
+      osc2: {
+        type: 'sine'
+      },
+      filter: filter,
+      osc1Gain: osc1Gain,
+      osc2Gain: osc2Gain,
     	noteOn: function(note, keyCode) {
     		if (!voices[keyCode]) {
     			//Create new voice and store it in voices array
--- app/scripts/voice.js
+++ app/scripts/voice.js
@@ -4,19 +4,30 @@ angular.module('rumca-js')
   .factory('Voice', function () {
 
     var Voice = function (note, dsp) {
-      this.osc = dsp.ctx.createOscillator();
-      this.osc.frequency.value = note;
-      this.osc.connect(dsp.ctx.destination);
-      this.osc.type = dsp.osc.type;
-      this.osc.start();
+
+      //create oscillator 1
+      this.osc1 = dsp.ctx.createOscillator();
+      this.osc1.frequency.value = note;
+      this.osc1.connect(dsp.osc1Gain);
+      this.osc1.type = dsp.osc1.type;
+      this.osc1.start();
+
+      //create oscillator 2
+      this.osc2 = dsp.ctx.createOscillator();
+      this.osc2.frequency.value = note;
+      this.osc2.connect(dsp.osc2Gain);
+      this.osc2.type = dsp.osc2.type;
+      this.osc2.start();
     };
 
     Voice.prototype.noteOff = function() {
-    	this.osc.stop();
+    	this.osc1.stop();
+      this.osc2.stop();
     };
 
     Voice.prototype.setOscType = function (value) {
-      this.osc.type = value;
+      this.osc1.type = value;
+      this.osc2.type = value;
     };
 
     return Voice;
