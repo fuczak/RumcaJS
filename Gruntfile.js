705814ef1278bc5b86dd6ce3047d696e22ec7aa7
Damian Prus <prus.damian@gmail.com>
2015-07-15 22:05:21 +0200

    fixed gruntfile bug

--- Gruntfile.js
+++ Gruntfile.js
@@ -352,7 +352,7 @@ module.exports = function (grunt) {
     ngtemplates: {
       dist: {
         options: {
-          module: 'webAppsApp',
+          module: 'rumca-js',
           htmlmin: '<%= htmlmin.dist.options %>',
           usemin: 'scripts/scripts.js'
         },
